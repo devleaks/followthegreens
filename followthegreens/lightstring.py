1a511987d823a74bd1b377b29b2630f3842eac0c
Pierre M <6030167+devleaks@users.noreply.github.com>
2026-02-07 16:33:50 +0100

    added alternate method to measure distance to closest (verification)

--- followthegreens/lightstring.py
+++ followthegreens/lightstring.py
@@ -850,6 +850,9 @@ class LightString:
         light = self.lights[ns]
         point = Point(position[0], position[1])
         d = distance(point, light.position)
+        c, d2 = self.closest(position)
+        d3 = abs(c - ns) * self.distance_between_lights
+        logger.debug(f"control: closest={c} (at {round(d2, 1)}m), next stop={ns}, d calc={round(d3, 1)}m, d mesure={round(d, 1)}m")
         return [ns, d]
 
     def offToIndex(self, idx):
